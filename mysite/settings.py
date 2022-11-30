b7413abc795f6637a8ec2cdb103e59505c932475
Anton Shefer <rezajkee@mail.ru>
2022-11-30 22:04:02 +0500

    Modify URL for posts

--- mysite/settings.py
+++ mysite/settings.py
@@ -20,7 +20,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-jto9pi2v6i$!bm)cg79s7h%(t-@+i9+)@^8crh33t(0o2ul&e^"
+SECRET_KEY = (
+    "django-insecure-jto9pi2v6i$!bm)cg79s7h%(t-@+i9+)@^8crh33t(0o2ul&e^"
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
