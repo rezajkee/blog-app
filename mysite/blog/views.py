effe193fa6f68e9a3d16be875576eba3cc47ea2b
Anton Shefer <rezajkee@mail.ru>
2022-12-03 17:48:29 +0500

    Change to searching with trigram similarity

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -8,7 +8,9 @@ import os
 from django.views.decorators.http import require_POST
 from taggit.models import Tag
 from django.db.models import Count
-from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
+# from django.contrib.postgres.search import
+# SearchVector, SearchQuery, SearchRank
+from django.contrib.postgres.search import TrigramSimilarity
 
 
 def post_list(request, tag_slug=None):
@@ -121,18 +123,23 @@ def post_search(request):
         form = SearchForm(request.GET)
         if form.is_valid():
             query = form.cleaned_data["query"]
-            # For RU add config="russian"
-            search_vector = SearchVector(
-                "title", weight="A"
-            ) + SearchVector(
-                "body", weight="B"
-            )
-            # For RU add config="russian"
-            search_query = SearchQuery(query)
+            # # For RU add config="russian"
+            # search_vector = SearchVector(
+            #     "title", weight="A"
+            # ) + SearchVector(
+            #     "body", weight="B"
+            # )
+            # # For RU add config="russian"
+            # search_query = SearchQuery(query)
+            # results = Post.published.annotate(
+            #     search=search_vector,
+            #     rank=SearchRank(search_vector, search_query)
+            # ).filter(rank__gte=0.3).order_by("-rank")
+
+            # Change to searching with trigram similarity
             results = Post.published.annotate(
-                search=search_vector,
-                rank=SearchRank(search_vector, search_query)
-            ).filter(rank__gte=0.3).order_by("-rank")
+                similarity=TrigramSimilarity("title", query)
+            ).filter(similarity__gt=0.1).order_by("-similarity")
     return render(
         request,
         "blog/post/search.html",
